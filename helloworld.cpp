f07be312b1d2aa12a8db66853a5f391f1c2ffb3b
Davood Soleymanzadeh <87722597+DavoodSZ1993@users.noreply.github.com>
2023-01-09 15:45:40 +0330

    Delete helloworld.cpp

--- helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>   /*Header File Library*/
-using namespace std;  /*Names for objects and variables from standard library*/
-
-int main(){
-
-    cout <<"Hello World!"; /*Semi-colon should be included!*/
-    return 0;
-}
