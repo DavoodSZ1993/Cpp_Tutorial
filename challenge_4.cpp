56fe7341b40bf52ad74325a29a607ffa5f7fbb75
Davood Soleymanzadeh <87722597+DavoodSZ1993@users.noreply.github.com>
2023-01-11 16:16:54 +0330

    Add files via upload

---
+++ challenge_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+int main(){
+
+    int num;
+
+    cout << "Enter your favorite number between 1 and 100: " <<endl;
+    cin >> num;
+
+    cout << "Amazing!! that's my favorite number too!!" <<endl;
+    cout <<"No really!! " << num <<" is my favorite number!"<<endl;
+
+    return 0;
+}
